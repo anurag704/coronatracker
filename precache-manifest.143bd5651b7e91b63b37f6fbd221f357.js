592d20dde0d4ec9070253d7d797649a2726ba65a
anurag704 <anuragjha1093@gmail.com>
2020-09-24 17:00:10 +0530

    Updates

---
+++ precache-manifest.143bd5651b7e91b63b37f6fbd221f357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d48a6dfaf10c26de512473c882367e",
+    "url": "/coronatracker/index.html"
+  },
+  {
+    "revision": "87daec5b8ef230024b6d",
+    "url": "/coronatracker/static/css/main.6dc3be1e.chunk.css"
+  },
+  {
+    "revision": "d06ce46fa0a6de603a31",
+    "url": "/coronatracker/static/js/2.3dc1acc6.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronatracker/static/js/2.3dc1acc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87daec5b8ef230024b6d",
+    "url": "/coronatracker/static/js/main.e607aabb.chunk.js"
+  },
+  {
+    "revision": "8482fb868c58831bd717",
+    "url": "/coronatracker/static/js/runtime-main.98898659.js"
+  },
+  {
+    "revision": "e55c3290cf8f3b10903b15186a9214a8",
+    "url": "/coronatracker/static/media/covid19.e55c3290.png"
+  }
+]);
